5508c91782fa4a50dca47698fb9d4067f6106835
ernestomiguez <ernestomiguez2010@gmail.com>
2011-09-04 13:01:15 +0200

    added oh-my-zsh

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 ln -s ~/dotifles/.screenrc ~/.screenrc
 ln -s ~/dotifles/.rails.screen ~/.rails.screen
 ln -s ~/dotifles/vimperatorrc ~/.vimperatorrc
+ln -s ~/dotifles/oh-my-zsh ~/.oh-my-zsh
+cp ~/.zshrc ~/.zshrc.orig 
+cp ~/dotfiles/oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 echo "links installed"
 exit 0
