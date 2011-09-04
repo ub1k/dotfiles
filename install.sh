59ecf83fadf14f41a418f84cb929d993518b1df2
ernestomiguez <ernestomiguez2010@gmail.com>
2011-09-04 13:12:23 +0200

    updated with zshenv

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 ln -s ~/dotifles/.screenrc ~/.screenrc
 ln -s ~/dotifles/.rails.screen ~/.rails.screen
 ln -s ~/dotifles/vimperatorrc ~/.vimperatorrc
+ln -s ~/dotifles/aliases ~/.aliases.sh
+ln -s ~/dotifles/zshenv ~/.zshenv
 #cp ~/.zshrc ~/.zshrc.orig 
 #wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 echo "links installed"
