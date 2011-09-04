8c8f51236d015643e436017f2f4502195ae3edae
emir <emir@matrix.(none)>
2011-09-04 13:06:55 +0200

    updated

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 ln -s ~/dotifles/.screenrc ~/.screenrc
 ln -s ~/dotifles/.rails.screen ~/.rails.screen
 ln -s ~/dotifles/vimperatorrc ~/.vimperatorrc
-ln -s ~/dotifles/oh-my-zsh ~/.oh-my-zsh
-cp ~/.zshrc ~/.zshrc.orig 
-wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
+#cp ~/.zshrc ~/.zshrc.orig 
+#wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
 echo "links installed"
 exit 0
