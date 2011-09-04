f6952ad96721a830cecb4a1e603abe20ea759f59
ernestomiguez <ernestomiguez2010@gmail.com>
2011-09-04 12:52:19 +0200

    updated install sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
-ln -s ~/dotifles/.screenrc .screenrc
-ln -s ~/dotifles/.rails.screen .rails.screen
-ln -s ~/dotifles/vimperatorrc .vimperatorrc
+ln -s ~/dotifles/.screenrc ~/.screenrc
+ln -s ~/dotifles/.rails.screen ~/.rails.screen
+ln -s ~/dotifles/vimperatorrc ~/.vimperatorrc
 echo "links installed"
 exit 0
